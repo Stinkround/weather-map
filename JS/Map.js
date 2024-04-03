84712dfb465ed5d474535b61f1c02552d01fba78
Stinkround <165833092+Stinkround@users.noreply.github.com>
2024-04-03 13:12:32 -0400

    Update Map.js

--- JS/Map.js
+++ JS/Map.js
@@ -64,7 +64,7 @@ async function createMap() {
     .bindPopup(
       `<h3>Your Location:</h3>` +
         `<h4>${location.city}, ${location.state}</h4>` +
-        `<br> <strong style="text-decoration: underline; cursor: pointer;" onclick="window.open('/weather-map/weather/weather.html', '_blank')">More Info..</strong>`
+        `<br> <strong style="text-decoration: underline; cursor: pointer;" onclick="window.open('/weather-map/Weather/weather.html', '_blank')">More Info..</strong>`
     )
     .openPopup();
 
